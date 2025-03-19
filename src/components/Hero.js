b66aea855fe6ed7d8b84cec78bdac9eeea6ca441
Sameeran4218 <n.sameeran@iitg.ac.in>
2025-03-19 16:02:18 +0530

    update

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -8,7 +8,7 @@ const Hero = () => {
         <img src={profilePic} alt="Sameeran" className="profile-img" />
         <h1>Hey, I'm <span className="highlight">Sameeran</span></h1>
         <p>Software Developer | Machine Learning Engineer | Data Scientist</p>
-        <a href="/resume.pdf" className="btn" download>Download Resume</a>
+        <a href="/resume.pdf" className="btn" download="Resume.pdf">Download Resume</a>
         <a href="#projects" className="btn">My Work</a>
       </div>
     </section>
