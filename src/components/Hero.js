73e7b1eee6a3e380b37fe3dec5c57f57bb81d3a2
Sameeran4218 <167286080+sameeran4218@users.noreply.github.com>
2025-03-19 18:10:53 +0530

    Update Hero.js

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,14 +1,6 @@
 import React from "react";
 import profilePic from "../assets/photo.jpg"; // Update with correct path
 
-const downloadResume = () => {
-  const link = document.createElement("a");
-  link.href = `${process.env.PUBLIC_URL}/resume.pdf`; // Ensure correct path
-  link.download = "Sameeran_Resume.pdf"; // Custom filename
-  document.body.appendChild(link);
-  link.click();
-  document.body.removeChild(link);
-};
 
 
 const Hero = () => {
@@ -19,7 +11,7 @@ const Hero = () => {
         <h1>Hey, I'm <span className="highlight">Sameeran</span></h1>
         <p>Software Developer | Machine Learning Engineer | Data Scientist</p>
         <a 
-  href="https://drive.google.com/file/d/1XfZ90UiXZbMq1PeOyOLsphS9OnIDsQOR/view?usp=drive_link" 
+  href="https://drive.google.com/file/d/13CF3lrCITT_uL979yP4m7WIr3ZbQsiBw/view?usp=drive_link" 
   className="btn" 
   target="_blank" 
   rel="noopener noreferrer"
